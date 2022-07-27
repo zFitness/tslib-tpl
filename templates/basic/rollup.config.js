22b2154e1cd5205f28d5ad649fe3f8dfd7196018
zfitness <zfitcode@163.com>
2022-07-27 13:01:07 +0800

    chore: basic

--- templates/basic/rollup.config.js
+++ templates/basic/rollup.config.js
@@ -1,82 +1,80 @@
-import path from "path";
-import esbuild from "rollup-plugin-esbuild";
-import babel from "rollup-plugin-babel";
-import { terser } from "rollup-plugin-terser";
-import resolve from "rollup-plugin-node-resolve";
-import dts from "rollup-plugin-dts";
+import path from 'path'
+import esbuild from 'rollup-plugin-esbuild'
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import resolve from 'rollup-plugin-node-resolve'
+import dts from 'rollup-plugin-dts'
 
 export default [
   {
-    input: path.resolve(__dirname, "./src/index.ts"),
+    input: path.resolve(__dirname, './src/index.ts'),
     output: [
       {
-        dir: path.resolve(__dirname, "esm"),
-        format: "esm",
+        file: path.resolve(__dirname, 'dist', 'index.esm.js'),
+        // dir: path.resolve(__dirname, 'esm'),
+        format: 'esm',
       },
     ],
     plugins: [
       resolve(),
       babel({
-        exclude: ["node_modules/**"],
+        exclude: ['node_modules/**'],
       }),
       esbuild({
-        target: "es2018",
+        target: 'es2018',
       }),
       terser(),
     ],
   },
   {
-    input: path.resolve(__dirname, "./src/index.ts"),
+    input: path.resolve(__dirname, './src/index.ts'),
     output: [
       {
-        dir: path.resolve(__dirname, "lib"),
-        format: "cjs",
+        file: path.resolve(__dirname, 'dist', 'index.cjs.js'),
+        // dir: path.resolve(__dirname, 'lib'),
+        format: 'cjs',
       },
     ],
     plugins: [
       babel({
-        exclude: "node_modules/**",
+        exclude: 'node_modules/**',
       }),
       esbuild({
-        target: "es2018",
+        target: 'es2018',
       }),
       terser(),
     ],
   },
   {
-    input: path.resolve(__dirname, "./src/index.ts"),
+    input: path.resolve(__dirname, './src/index.ts'),
     output: [
       {
         file: path.resolve(__dirname, `dist/index.umd.production.js`),
-        name: "template",
-        format: "umd",
+        name: 'template',
+        format: 'umd',
         sourcemap: true,
       },
     ],
     external: [],
     plugins: [
       babel({
-        exclude: "node_modules/**",
+        exclude: 'node_modules/**',
       }),
       esbuild({
-        target: "es2018",
+        target: 'es2018',
       }),
       terser(),
     ],
   },
   {
-    input: path.resolve(__dirname, "./src/index.ts"),
+    input: path.resolve(__dirname, './src/index.ts'),
     output: [
       {
-        dir: path.resolve(__dirname, "esm"),
-        format: "esm",
-      },
-      {
-        dir: path.resolve(__dirname, "lib"),
-        format: "cjs",
-      },
+        dir: path.resolve(__dirname, 'dist'),
+        format: 'esm',
+      }
     ],
     external: [],
     plugins: [dts({ respectExternal: true })],
   },
-];
+]
